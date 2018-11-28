63c24b2e395def8b3d7c4f5bfbc03b7af80f5a1c
github username <github email address>
2018-11-28 17:51:28 -0600

    form message and type done

--- spamjam_frontend/src/App.js
+++ spamjam_frontend/src/App.js
@@ -33,7 +33,30 @@ class App extends Component {
   makeTypeOptions(){
     //map all these into <options>
   }
+
+  handleChange = (e) => {
+    const foundType = this.state.allSpamTypes.find( type => {
+      return type.name === e.target.value
+    })
+    this.setState({
+      spamType: foundType
+    })
+  }//handleChange
+
+  handleSubmit = (e) => {
+
+  }//handleSubmit
+
   render() {
+    const style = {
+      maxHeight:'400px',
+      minHeight:'400px',
+      resize:'none',
+      padding:'9px',
+      boxSizing:'border-box',
+      fontSize:'15px'
+    };
+
     if(this.state.loaded){
       return (
         <div className="App">
@@ -42,24 +65,32 @@ class App extends Component {
             <div>
               <h1>SPAM JAM 2.0</h1>
             </div>
-            <div>
-              <label>Victim Email</label>
-              <input type="text" placeholder="someone@something.com" />
-            </div>
-            <div>
-              <input type="textarea" width="50" height="30" />
-            </div>
-            <div>
-              <select>
-                {this.state.allSpamTypes.map(type => (
-                  <option key={type.name} value={type.name}>{type.name}</option>
-                ))}
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option selected value="coconut">Coconut</option>
-                <option value="mango">Mango</option>
-              </select>
-            </div>
+            <form onSubmit={this.handleSubmit}>
+              <div>
+                <label>Victim Email</label>
+                <input type="text" placeholder="someone@something.com" />
+              </div>
+              <div>
+                <select onChange={this.handleChange}>
+                  <option key="blank" value="blank" default></option>
+                  {this.state.allSpamTypes.map(type => (
+                    <option key={type.name} value={type.name}>{type.name}</option>
+                  ))}
+                </select>
+              </div>
+              <br></br>
+              <div>
+                <textarea
+                  style={style}
+                  placeholder="message"
+                  value={this.state.spamType != null ? this.state.spamType.template : ""}
+                />
+              </div>
+              <div>
+                <input type="submit" value="Submit" />
+              </div>
+            </form>
+
 
 
           </header>
